2fe04a0200a87620321900ce604fdc7d2605a196
Tural Hasanov <hasanovtural11@gmail.com>
2021-01-29 12:38:46 +0400

    admin middleware setup. quiz migration, factory, seeder setup. user factory, seeder setup.

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+Route::prefix('admin')->name('admin.')->middleware(['auth','isAdmin'])->group(function(){
+    Route::get('');
+});
